70fe980e0d8ba172ca1add31d88a9bce7bd0f76d
yoong <y78462@gmail.com>
2021-03-23 14:00:51 +0900

    collision

--- app/src/main/java/com/example/untactshop/Map.java
+++ app/src/main/java/com/example/untactshop/Map.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
-public class map extends AppCompatActivity {
+public class Map extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
