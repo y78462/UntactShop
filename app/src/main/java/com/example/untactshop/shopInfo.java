696e5e825398abfeca11632e2d5c6307b4f8411e
yoong <y78462@gmail.com>
2021-04-04 15:07:40 +0900

    Realm test

---
+++ app/src/main/java/com/example/untactshop/shopInfo.java
@@ -0,0 +1,89 @@
+package com.example.untactshop;
+
+import io.realm.RealmObject;
+
+public class shopInfo extends RealmObject {
+    private String station_name; //역이름
+    private String shop_name; //가게이름
+    private String room_name; //호수
+    private String category;
+
+    //좌표
+    private double x1;
+    private double x2;
+    private double y1;
+    private double y2;
+
+    public shopInfo(String station_name, String shop_name, String room_name, String category) {
+        this.station_name = station_name;
+        this.shop_name = shop_name;
+        this.room_name = room_name;
+        this.category = category;
+    }
+
+    public void setStation_name(String station_name) {
+        this.station_name = station_name;
+    }
+
+    public void setShop_name(String shop_name) {
+        this.shop_name = shop_name;
+    }
+
+    public void setRoom_name(String room_name) {
+        this.room_name = room_name;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setX1(double x1) {
+        this.x1 = x1;
+    }
+
+    public void setX2(double x2) {
+        this.x2 = x2;
+    }
+
+    public void setY1(double y1) {
+        this.y1 = y1;
+    }
+
+    public void setY2(double y2) {
+        this.y2 = y2;
+    }
+
+    public String getStation_name() {
+        return station_name;
+    }
+
+    public String getShop_name() {
+        return shop_name;
+    }
+
+    public String getRoom_name() {
+        return room_name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public double getX1() {
+        return x1;
+    }
+
+    public double getX2() {
+        return x2;
+    }
+
+    public double getY1() {
+        return y1;
+    }
+
+    public double getY2() {
+        return y2;
+    }
+
+
+}
---
+++ app/src/main/java/com/example/untactshop/test_class.java
@@ -0,0 +1,63 @@
+package com.example.untactshop;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+import io.realm.RealmResults;
+
+
+public class test_class extends AppCompatActivity {
+
+
+    Realm realm;
+
+    public shopInfo test1 = new shopInfo("강남역","비프루브 강남역점","A-1,2","쇼핑미용");
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.test_layout);
+        Realm.init(this);
+        RealmConfiguration config = new RealmConfiguration.Builder()
+                .name("ShopInfo.realm") //생성할 realm파일 이름 지정
+                .schemaVersion(0)
+                .build();
+        //Realm에 셋팅한 정보 값을 지정
+        Realm.setDefaultConfiguration(config);
+
+
+        //transaction
+        realm = Realm.getDefaultInstance();
+        addData();
+        realm.commitTransaction();
+
+        //select
+        shopInfo shop = realm.where(shopInfo.class).equalTo("category", "쇼핑미용").findFirst(); //특정 id가진 데이터불러오기
+
+        TextView t1 = findViewById(R.id.textbox);
+        t1.setText(shop.getShop_name());
+
+    }
+
+    private void addData(){
+        realm.executeTransaction(new Realm.Transaction() {
+            @Override
+            public void execute(Realm realm) {
+                //Realm에 생성한 test를 저장하는 코드
+                realm.copyToRealm(test1);
+            }
+        });
+    }
+
+    private RealmResults getDiaryList(){
+        //Realm에 저장된 Diary들을 모두 찾아달라고 Realm에 요청해서 받아오는 코드입니다
+        RealmResults diaryRealmResults = realm.where(shopInfo.class).findAll();
+
+        return diaryRealmResults;
+    }
+
+
+}
