9cda11ffe09508b052cf2650e80e25fc5134c1c3
HwangJaeHwan <zxcv0069@naver.com>
2021-04-06 21:29:12 +0900

    test DB로 객체 생성
    ...

--- app/src/main/java/com/example/untactshop/Map.java
+++ app/src/main/java/com/example/untactshop/Map.java
@@ -1,13 +1,15 @@
 package com.example.untactshop;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.widget.TextView;
 
-import com.example.untactshop.R;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.List;
 
 public class Map extends AppCompatActivity {
+
+    public List<shopInfo> shopList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +17,30 @@ public class Map extends AppCompatActivity {
         setContentView(R.layout.activity_map);
 
         TextView name = this.findViewById(R.id.textView);
+        TextView name1 = this.findViewById(R.id.textView1);
+        TextView name2 = this.findViewById(R.id.textView2);
+        TextView name3 = this.findViewById(R.id.textView3);
+
         name.setText(getIntent().getStringExtra("name"));
+
+        initLoadDB();
+        name1.setText(shopList.get(0).getShop_name()+shopList.get(0).getRoom_name()+shopList.get(0).getCategory());
+        name2.setText(shopList.get(999).getShop_name()+shopList.get(999).getRoom_name()+shopList.get(999).getCategory());
+        name3.setText(shopList.get(1999).getShop_name()+shopList.get(1999).getRoom_name()+shopList.get(1999).getCategory());
+
+
+    }
+
+    private void initLoadDB() {
+
+        DataAdapter mDbHelper = new DataAdapter(getApplicationContext());
+        mDbHelper.createDatabase();
+        mDbHelper.open();
+
+        // db에 있는 값들을 model을 적용해서 넣는다.
+        shopList = mDbHelper.getTableData();
+
+        // db 닫기
+        mDbHelper.close();
     }
 }
